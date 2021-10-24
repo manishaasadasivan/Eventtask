9b94e6e661c8f45ab9a0d0ef3b848a2bcdae6043
manishaasadasivan <89304644+manishaasadasivan@users.noreply.github.com>
2021-10-24 15:15:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function display(){
+    let name=document.getElementById("name").value;
+document.getElementById("out").innerText="Thanks "+name+"for Registering will send you confirmation mail";
+}
+function displayy(){
+    let name=document.getElementById("Sname").value;
+document.getElementById("out1").innerText="Thanks "+name+"for Registering will send you confirmation mail";
+}
